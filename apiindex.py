fe96cecb214e229f36eaf6f004fc095d55336743
Jeremy Nathanael Sidabutar <112705798+Northenwest@users.noreply.github.com>
2026-06-03 02:15:04 +0700

    Delete apiindex.py

--- apiindex.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import streamlit.web.cli as stcli
-
-def handler(request):
-    # Mengarahkan argumen untuk menjalankan app.py Anda
-    sys.argv = ["streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
-    stcli.main()
